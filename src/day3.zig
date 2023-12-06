c9c0682a194a1891124e371ded062b85888520d0
Mifom <requestdg@gmail.com>
2023-12-06 21:36:13 +0300

    Optimise day 3

--- src/day3.zig
+++ src/day3.zig
@@ -17,33 +17,31 @@ const Field = struct {
         var symbols = std.ArrayList(Symbol).init(alloc);
         var row: usize = 0;
         while (lines.next()) |line| {
-            // numbers
             {
                 var pos: usize = 0;
                 while (pos < line.len) {
-                    const start = std.mem.indexOfAnyPos(u8, line, pos, "1234567890") orelse break;
-                    const end = std.mem.indexOfNonePos(u8, line, start, "1234567890") orelse line.len;
-                    pos = end;
-                    numbers.append(.{
-                        .value = std.fmt.parseInt(u32, line[start..end], 10) catch unreachable,
-                        .row = row,
-                        .col = start,
-                        .len = end - start,
-                    }) catch unreachable;
-                }
-            }
-            // symbols
-            {
-                var pos: usize = 0;
-                while (pos < line.len) {
-                    const symbol = std.mem.indexOfNonePos(u8, line, pos, "1234567890.") orelse break;
-                    const isGear = line[symbol] == '*';
-                    pos = symbol + 1;
-                    symbols.append(.{
-                        .row = row,
-                        .col = symbol,
-                        .isGear = isGear,
-                    }) catch unreachable;
+                    const start = std.mem.indexOfNonePos(u8, line, pos, ".") orelse break;
+
+                    if (line[start] >= '0' and line[start] <= '9') {
+                        // numbers
+                        const end = std.mem.indexOfNonePos(u8, line, start, "1234567890") orelse line.len;
+                        pos = end;
+                        numbers.append(.{
+                            .value = std.fmt.parseInt(u32, line[start..end], 10) catch unreachable,
+                            .row = row,
+                            .col = start,
+                            .len = end - start,
+                        }) catch unreachable;
+                    } else {
+                        // symbols
+                        const isGear = line[start] == '*';
+                        pos = start + 1;
+                        symbols.append(.{
+                            .row = row,
+                            .col = start,
+                            .isGear = isGear,
+                        }) catch unreachable;
+                    }
                 }
             }
 
@@ -74,8 +72,18 @@ pub fn part1(alloc: std.mem.Allocator, input: []const u8) u32 {
     const field = Field.parse(alloc, input);
     defer field.deinit();
     for (field.numbers.items) |number| {
+        var near_row = false;
         for (field.symbols.items) |symbol| {
-            if (dist(symbol.row, number.row) <= 1 and symbol.col + 1 >= number.col and symbol.col <= number.col + number.len) {
+            if (dist(symbol.row, number.row) <= 1) {
+                near_row = true;
+            } else {
+                if (near_row) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            if (symbol.col + 1 >= number.col and symbol.col <= number.col + number.len) {
                 sum += number.value;
                 break;
             }
@@ -91,8 +99,21 @@ pub fn part2(alloc: std.mem.Allocator, input: []const u8) u32 {
     for (field.symbols.items) |symbol| {
         var ratio: u32 = 1;
         var n: u32 = 0;
+        var near_row = false;
         for (field.numbers.items) |number| {
-            if (dist(symbol.row, number.row) <= 1 and symbol.col + 1 >= number.col and symbol.col <= number.col + number.len) {
+            if (dist(symbol.row, number.row) <= 1) {
+                near_row = true;
+            } else {
+                if (near_row) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            if (symbol.col + 1 >= number.col and symbol.col <= number.col + number.len) {
+                if (n == 2) {
+                    break;
+                }
                 ratio *= number.value;
                 n += 1;
             }
